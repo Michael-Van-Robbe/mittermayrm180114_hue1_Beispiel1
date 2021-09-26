b2d58c93ce4553ea6df673ef2de2c4e7cd321e5f
Michael-Van-Robbe <mittermayrm180114@sus.htl-grieskirchen.at>
2021-09-26 20:59:55 +0200

    main method was added to test the "isPrime" and "printPrimes" methods

--- src/net/htlgkr/mittermayrm180114/beispiel1/Main.java
+++ src/net/htlgkr/mittermayrm180114/beispiel1/Main.java
@@ -2,5 +2,17 @@ package net.htlgkr.mittermayrm180114.beispiel1;
 
 public class Main
 {
+    public static void main(String[] args)
+    {
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(30);
 
+        //test for "isPrime" method
+        for(int i = 1; i <= 30; i++)
+        {
+            System.out.println("Number: " + i + ", " + eps.isPrime(i));
+        }
+
+        //test for "printPrimes" method
+        eps.printPrimes();
+    }
 }
