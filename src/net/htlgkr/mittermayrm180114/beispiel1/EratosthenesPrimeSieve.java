6be9460de7bdf69b7aefa09b36d3e4f5cd353447
Michael-Van-Robbe <mittermayrm180114@sus.htl-grieskirchen.at>
2021-09-26 20:51:29 +0200

    "isPrime" method was made

--- src/net/htlgkr/mittermayrm180114/beispiel1/EratosthenesPrimeSieve.java
+++ src/net/htlgkr/mittermayrm180114/beispiel1/EratosthenesPrimeSieve.java
@@ -5,7 +5,22 @@ public class EratosthenesPrimeSieve implements PrimeSieve
     @Override
     public boolean isPrime(int p)
     {
-        return false;
+        boolean isPrime = true;
+
+        if(p <= 1)
+        {
+            isPrime = false;
+        }
+
+        for(int i = 2; i < p; i++)
+        {
+            if(p % i == 0)
+            {
+                isPrime = false;
+            }
+        }
+
+        return isPrime;
     }
 
     @Override
